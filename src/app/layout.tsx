30b0957f2a405f1b8fa12fec647e008128b055d0
israfil-hossain <Israfil166091@gmail.com>
2026-08-25 02:37:06 +0600

    feat: add Google Tag Manager (GTM-W3LBBK9P) with noscript fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,16 @@ export default function RootLayout({
             <head>
                 <StructuredData />
                 <Script
+                    id="gtm-script"
+                    strategy="afterInteractive"
+                >
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W3LBBK9P');`}
+                </Script>
+                <Script
                     src="https://www.googletagmanager.com/gtag/js?id=G-G0S8BJ2SL0"
                     strategy="afterInteractive"
                 />
@@ -87,6 +97,14 @@ export default function RootLayout({
                 </Script>
             </head>
             <body className="min-h-screen bg-cream text-dark antialiased font-bangla">
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-W3LBBK9P"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 <Toaster richColors position="top-right" />
                 {children}
             </body>
